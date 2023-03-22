2da27ddc728d56f3b3289ac95ed04d50caef1cbf
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-22 21:39:25 +0000

    Completed javascript-conditonals exercise

---
+++ javascript-conditionals/javascript-conditionals-quiz-notes.md
@@ -0,0 +1,49 @@
+# javascript-conditionals-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- Give 6 examples of comparison operators.
+<, >, <=, =>, ==, !=
+- What data type do comparison expressions evaluate to?
+Boolean data types are the result of comparison expressions.
+- What is the purpose of an `if` statement?
+An if statement gives conditions in order for a following code block to run
+- Is `else` required in order to use an `if` statement?
+No, it is not.
+- Describe the syntax (structure) of an `if` statement.
+An if statement consists of the if keyword, the conditonal wrapped in parantheses, and the code block wrapped in curly braces.
+- What are the three logical operators?
+&&, || and !. Respectively, they are the AND, OR and NOT operators.
+- How do you compare two different expressions in the same condition?
+You wrap each expression in their own pair of parentheses and place a comparison operator between them.
+- What is the purpose of a `switch` statement?
+The switch statement is useful instead of an if-else if statement, especially if there are multiple cases we want to check.
+- Is a `default` clause required in order to use a `switch` statement?
+No, it is not.
+## Notes
+
+All student notes should be written here.
+
+
+How to write `Code Examples` in markdown
+
+for JS:
+```javascript
+const data = "Howdy"
+```
+
+for HTML:
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+```css
+div {
+  width:100%
+}
+```
---
+++ javascript-conditionals/main.js
@@ -0,0 +1,81 @@
+/* exported isUnderFive,
+            isEven,
+            startsWithJ,
+            isOldEnoughToDrink,
+            isOldEnoughToDrive,
+            isOldEnoughToDrinkAndDrive,
+            categorizeAcidity,
+            introduceWarnerBro,
+            recommendMovie
+ */
+
+const isUnderFive = (num) => {
+  return (num < 5);
+};
+
+const isEven = (num) => {
+  return (num % 2 === 0);
+};
+
+const startsWithJ = (str) => {
+  return (str[0] === 'J');
+};
+
+const isOldEnoughToDrink = (person) => {
+  return (person.age > 20);
+};
+
+const isOldEnoughToDrive = (person) => {
+  return (person.age > 15);
+};
+
+const isOldEnoughToDrinkAndDrive = (person) => {
+  return false;
+};
+
+const categorizeAcidity = (pH) => {
+  if (pH === 7) {
+    return 'neutral';
+  } else if (pH < 7 && pH > 0) {
+    return 'acid';
+  } else if (pH < 14 && pH > 7) {
+    return 'base';
+  } else {
+    return 'invalid pH level';
+  }
+};
+
+const introduceWarnerBro = (name) => {
+  if (name === 'yakko' || name === 'wakko') {
+    return "We're the warner brothers!";
+  } else if (name === 'dot') {
+    return "I'm cute~";
+  } else {
+    return 'Goodnight Everybody!';
+  }
+};
+
+const recommendMovie = (genre) => {
+  switch (genre) {
+    case 'action':
+      return 'Puss in Boots';
+
+    case 'comedy':
+      return 'Rush Hour';
+
+    case 'horror':
+      return 'The Ring';
+
+    case 'drama':
+      return 'Lion';
+
+    case 'musical':
+      return 'La La Land';
+
+    case 'sci-fi':
+      return 'Wall-E';
+
+    default:
+      return 'Genre not recognized. Choose between action, comedy, horror, drama, musical, or sci-fi';
+  }
+};
