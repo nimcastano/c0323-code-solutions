3dd8f06012ba1b016bfe699ad95bc47db0c00123
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-27 19:08:22 +0000

    Completed string manipulation 3

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -25,9 +25,25 @@ const titleCase = title => {
       }
       cappedWords.push(js);
     } else if (words[i].toLowerCase() === 'api') {
-      cappedWords.push('API');
+      let api = 'API';
+      if (words[i].length > api.length) {
+        api += words[i][words[i].length - 1];
+      }
+      cappedWords.push(api);
     } else if (words[i].length <= 3 && i !== 0 && conjun[words[i]]) {
       cappedWords.push(words[i].toLowerCase());
+    } else if (words[i].includes('-')) {
+      const hyphen = words[i].indexOf('-');
+      let str = '';
+      str += words[i][0].toUpperCase();
+      for (let j = 1; j < hyphen + 1; j++) {
+        str += words[i][j];
+      }
+      str += words[i][hyphen + 1].toUpperCase();
+      for (let j = hyphen + 2; j < words[i].length; j++) {
+        str += words[i][j];
+      }
+      cappedWords.push(str);
     } else {
       let str = '';
       str += words[i][0].toUpperCase();
@@ -37,14 +53,13 @@ const titleCase = title => {
       cappedWords.push(str);
     }
     if (words[i].includes(':')) {
-      const str = words[i + 1];
-      let cappedStr = str[0].toUpperCase();
-      for (let j = 1; j < str.length; j++) {
-        cappedStr += str[j];
+      const notStr = words[i + 1];
+      let str = notStr[0].toUpperCase();
+      for (let j = 1; j < notStr.length; j++) {
+        str += notStr[j];
       }
-      console.log(cappedStr);
-      cappedWords.push(cappedStr);
       i++;
+      cappedWords.push(str);
     }
   }
   return cappedWords.join(' ');
