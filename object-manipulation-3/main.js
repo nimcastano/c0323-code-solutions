1fad672ab57225cb2e93c89e2ece41ce3a81656a
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-27 16:04:47 +0000

    object manipulation 3 progress

---
+++ object-manipulation-3/main.js
@@ -0,0 +1,36 @@
+// console.log('Lodash is loaded:', typeof _ !== 'undefined');
+// const player1 = {
+//   name: 'Leo',
+//   hand: []
+// };
+// const player2 = {
+//   name: 'Mikey',
+//   hand: []
+// };
+// const player3 = {
+//   name: 'Donnie',
+//   hand: []
+// };
+// const player4 = {
+//   name: 'Raf',
+//   hand: []
+// };
+
+// // const players = [player1, player2, player3, player4];
+
+// const cards = [];
+
+// const cardVals = ['Ace', 2, 3, 4, 5, 6, 7, 8, 9, 10, 'Jack', 'Queen', 'King'];
+
+// const cardSuits = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
+
+// let cardNum = 0;
+// for (let i = 0; i < cardVals.length; i++) {
+//   for (let j = 0; j < cardSuits.length; j++) {
+//     cards[cardNum].rank = cardVals[i];
+//     cards[cardNum].suit = cardSuits[j];
+//     cardNum++;
+//   }
+// }
+
+// console.log(cards);
