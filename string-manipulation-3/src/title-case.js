b6c4440c31ec4fd9bc1ddf890c0de36b70903805
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-27 19:20:37 +0000

    added comments for titlecase

--- string-manipulation-3/src/title-case.js
+++ string-manipulation-3/src/title-case.js
@@ -14,25 +14,30 @@ const titleCase = title => {
     on: true,
     to: true
   };
+  // split words up
   const words = title.toLowerCase().split(' ');
-  console.log(words);
+  // setup resulting array of words
   const cappedWords = [];
   for (let i = 0; i < words.length; i++) {
+    // to fix javascript string
     if (words[i].toLowerCase().includes('javascript')) {
       let str = 'JavaScript';
       if (words[i].length > str.length) {
         str += words[i][words[i].length - 1];
       }
       cappedWords.push(str);
-    } else if (words[i].toLowerCase() === 'api') {
+      // to fix api string
+    } else if (words[i].toLowerCase().includes('api')) {
       let str = 'API';
       if (words[i].length > str.length) {
         str += words[i][words[i].length - 1];
       }
       cappedWords.push(str);
+      // to fix conjunction words
     } else if (words[i].length <= 3 && i !== 0 && conjun[words[i]]) {
       const str = words[i].toLowerCase();
       cappedWords.push(str);
+      // to fix hyphenated words
     } else if (words[i].includes('-')) {
       const hyphen = words[i].indexOf('-');
       let str = '';
@@ -45,6 +50,7 @@ const titleCase = title => {
         str += words[i][j];
       }
       cappedWords.push(str);
+      // to fix any other word
     } else {
       let str = '';
       str += words[i][0].toUpperCase();
@@ -53,15 +59,18 @@ const titleCase = title => {
       }
       cappedWords.push(str);
     }
+    // to fix words after a word with a colon
     if (words[i].includes(':')) {
       const notStr = words[i + 1];
       let str = notStr[0].toUpperCase();
       for (let j = 1; j < notStr.length; j++) {
         str += notStr[j];
       }
+      // so the word after the colon doesnt go through the loop since we just fixed it
       i++;
       cappedWords.push(str);
     }
   }
+  // join the array of strings so it results into one big string
   return cappedWords.join(' ');
 };
