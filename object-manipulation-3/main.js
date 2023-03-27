a5ba367df7d48d26606aa899e74b2b243378f6bb
Nimrod Castano <nimrodcastano@gmail.com>
2023-03-27 21:23:23 +0000

    updated loop for cards

--- object-manipulation-3/main.js
+++ object-manipulation-3/main.js
@@ -25,12 +25,13 @@
 // const cardSuits = ['Clubs', 'Spades', 'Hearts', 'Diamonds'];
 
 // let cardNum = 0;
+// // Object.create()
 // for (let i = 0; i < cardVals.length; i++) {
 //   for (let j = 0; j < cardSuits.length; j++) {
-//     cards[cardNum].rank = cardVals[i];
-//     cards[cardNum].suit = cardSuits[j];
+//     cards[cardNum] = {
+//       rank: cardVals[i],
+//       suit: cardSuits[j]
+//     };
 //     cardNum++;
 //   }
 // }
-
-// console.log(cards);
