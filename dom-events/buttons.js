9137526139cdae0451805c2b06ad52717e7d60e1
Nimrod Castano <nimrodcastano@gmail.com>
2023-04-03 19:44:08 +0000

    completed dom-events exercise

---
+++ dom-events/buttons.js
@@ -0,0 +1,29 @@
+const handleClick = (event) => {
+  console.log('button clicked');
+  console.log('value of event: ', event);
+  console.log('value of target: ', event.target);
+};
+
+const $click = document.querySelector('.click-button');
+
+$click.addEventListener('click', handleClick, false);
+
+const handleMouseover = (event) => {
+  console.log('button hovered');
+  console.log('value of event: ', event);
+  console.log('value of target: ', event.target);
+};
+
+const $hover = document.querySelector('.hover-button');
+
+$hover.addEventListener('mouseover', handleMouseover, false);
+
+const handleDoubleClick = (event) => {
+  console.log('button double-clicked');
+  console.log('value of event: ', event);
+  console.log('value of target: ', event.target);
+};
+
+const $double = document.querySelector('.double-click-button');
+
+$double.addEventListener('dblclick', handleDoubleClick, false);
