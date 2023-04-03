ccbc66ed9fe0ff5a08d34c1daddca091258ac636
Nimrod Castano <nimrodcastano@gmail.com>
2023-04-03 17:29:02 +0000

    updated with labels

--- dom-querying/main.js
+++ dom-querying/main.js
@@ -1,19 +1,19 @@
 console.log('hello, world');
 
 const h1 = document.querySelector('h1');
-console.log(h1);
+console.log('heading: ', h1);
 console.dir(h1);
 
 const explan = document.querySelector('#explanation');
-console.log(explan);
+console.log('explanation ID: ', explan);
 console.dir(explan);
 
 const hint = document.querySelector('.hint');
-console.log(hint);
+console.log('hint class: ', hint);
 console.dir(hint);
 
 const p = document.querySelectorAll('p');
-console.log(p);
+console.log('paragraph tag: ', p);
 
 const exampleLink = document.querySelectorAll('.example-link');
-console.log(exampleLink);
+console.log('example-link class: ', exampleLink);
