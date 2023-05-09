0fb05f31b8bfbbe3a79c52042583d39bb0b7346f
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-09 17:56:34 +0000

    updated map for shorthand

--- array-methods/map.js
+++ array-methods/map.js
@@ -1,15 +1,7 @@
 const prices = [42.42, 10, 28.2234, 3.2, 5, 12];
 
-const sale = prices.map((el) => {
-  const obj = {
-    price: el,
-    salePrice: el / 2,
-  };
-  return obj;
-});
+const sale = prices.map((el) => ({ prices: el, salePrice: el / 2 }));
 console.log('price objects: ', sale);
 
-const formatted = prices.map((el) => {
-  return `$${el.toFixed(2)}`;
-});
+const formatted = prices.map((el) => `$${el.toFixed(2)}`);
 console.log('formatted prices: ', formatted);
