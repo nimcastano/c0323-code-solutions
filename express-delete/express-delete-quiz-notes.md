c8e4e157093ab7e7638ba7c6275800e95507f192
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-15 20:53:41 +0000

    completed express-delete exercise

---
+++ express-delete/express-delete-quiz-notes.md
@@ -0,0 +1,36 @@
+# express-delete-quiz-notes
+
+## Quiz Questions
+
+After completing this exercise, you should be able to discuss or answer the following questions:
+
+- What is the significance of an HTTP request's method?
+  They all allow you to do different things. It all just depends on what you actually want to do when you are requesting.
+
+## Notes
+
+All student notes should be written here.
+
+How to write `Code Examples` in markdown
+
+for JS:
+
+```javascript
+const data = 'Howdy';
+```
+
+for HTML:
+
+```html
+<div>
+  <p>This is text content</p>
+</div>
+```
+
+for CSS:
+
+```css
+div {
+  width: 100%;
+}
+```
---
+++ express-delete/server.js
@@ -0,0 +1,42 @@
+import express from 'express';
+
+const grades = {
+  12: {
+    id: 12,
+    name: 'Tim Berners-Lee',
+    course: 'HTML',
+    score: 95,
+  },
+  47: {
+    id: 47,
+    name: 'Brendan Eich',
+    course: 'JavaScript',
+    score: 100,
+  },
+  273: {
+    id: 273,
+    name: 'Forbes Lindsay',
+    course: 'JavaScript',
+    score: 92,
+  },
+};
+
+const app = express();
+
+app.get('/api/grades', (req, res) => {
+  const gradesArr = [];
+  for (const grade in grades) {
+    gradesArr.push(grades[grade]);
+  }
+  res.json(gradesArr);
+});
+
+app.delete('/api/grades/:id', (req, res) => {
+  const id = req.params.id;
+  delete grades[id];
+  res.sendStatus(204);
+});
+
+app.listen(8080, () => {
+  console.log('server is listening to port 8080');
+});
