7bc2f67738fd63533488678ab11eff17ef0d6fce
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-17 19:36:10 +0000

    simple change

--- sql-aggregates/starring-in.sql
+++ sql-aggregates/starring-in.sql
@@ -6,4 +6,4 @@ join "films" using ("filmId")
 join "castMembers" using ("filmId")
 join "actors" using ("actorId")
 where "actors"."firstName" = 'Lisa' and "actors"."lastName" = 'Monroe'
-group by "genres"."name"
+group by "genres"."genreId"
