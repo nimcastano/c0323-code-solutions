da2df85f5e7d1b20b6f186ea38fb87e625dffedd
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-23 18:45:46 +0000

    edits for readability

--- react-validated-input-component/nimmys-app/src/ValidatedInput.jsx
+++ react-validated-input-component/nimmys-app/src/ValidatedInput.jsx
@@ -3,25 +3,25 @@ import { useState } from 'react';
 export default function ValidatedInput() {
   const [password, setPassword] = useState('');
 
-  let icon = <i className="fa-solid fa-xmark" style={{ color: 'red' }}></i>;
+  const xmark = <i className="fa-solid fa-xmark" style={{ color: 'red' }}></i>;
   const check = (
     <i className="fa-solid fa-check" style={{ color: 'green' }}></i>
   );
 
-  let error = 'A password is required.';
+  const error =
+    !/\d/.test(password) && password.length >= 8
+      ? 'Password must contain a number.'
+      : !/[A-Z]/.test(password) && password.length >= 8
+      ? 'Password must contain an uppercase letter.'
+      : !/[!@#$%^&*()]/.test(password) && password.length >= 8
+      ? 'Password must contain a special character.'
+      : password.length < 8 && password.length > 0
+      ? 'Your password is too short.'
+      : password.length >= 8
+      ? ''
+      : 'A password is required.';
 
-  if (!/\d/.test(password) && password.length >= 8) {
-    error = 'Password must contain a number.';
-  } else if (!/[A-Z]/.test(password) && password.length >= 8) {
-    error = 'Password must contain an uppercase letter.';
-  } else if (!/[!@#$%^&*()]/.test(password) && password.length >= 8) {
-    error = 'Password must contain a special character.';
-  } else if (password.length < 8 && password.length > 0) {
-    error = 'Your password is too short.';
-  } else if (password.length >= 8) {
-    icon = check;
-    error = '';
-  }
+  const icon = error === '' ? check : xmark;
 
   return (
     <form className="form">
@@ -35,7 +35,7 @@ export default function ValidatedInput() {
           onChange={(e) => setPassword(e.target.value)}></input>
         <span>{icon}</span>
       </div>
-      <p> {error}</p>
+      <p>{error}</p>
     </form>
   );
 }
