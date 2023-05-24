1f11434579b848617d0882b169d4f51a0ebd9b80
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-24 17:30:48 +0000

    updated to be resuable and data-driven

--- react-accordion-component/nimmys-app/src/App.js
+++ react-accordion-component/nimmys-app/src/App.js
@@ -1,56 +1,75 @@
 import { useState } from 'react';
 import './App.css';
 
-function Container() {
-  const [activeIndex, setActiveIndex] = useState(undefined);
+const pages = [
+  {
+    key: 0,
+    title: 'Hypertext Markup Language',
+    descrip: `The HyperText Markup Language or HTML is the standard markup language
+          for documents designed to be displayed in a web browser. It is often assisted
+          by technologies such as Cascading Style Sheets (CSS) and scripting languages
+          such as JavaScript.`,
+  },
+  {
+    key: 1,
+    title: 'Cascading Style Sheets',
+    descrip: `Cascading Style Sheets (CSS) is a style sheet language used for describing
+          the presentation of a document written in a markup language such as HTML or XML
+          (including XML dialects such as SVG, MathML or XHTML). CSS is a cornerstone 
+          technology of the World Wide Web, alongside HTML and JavaScript.`,
+  },
+  {
+    key: 2,
+    title: 'JavaScript',
+    descrip: `JavaScript, often abbreviated as JS, is a programming language that is one of 
+          the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 
+          98% of websites use JavaScript on the client side for webpage behavior, often 
+          incorporating third-party libraries. All major web browsers have a dedicated 
+          JavaScript engine to execute the code on devices.`,
+  },
+];
 
+function App() {
   return (
     <div className="Container">
-      <Panel
-        title="Hypertext Markup Language"
-        descrip="The HyperText Markup Language or HTML is the standard markup language
-                for documents designed to be displayed in a web browser. It is often assisted
-                by technologies such as Cascading Style Sheets (CSS) and scripting languages
-                such as JavaScript."
-        isActive={activeIndex === 0}
-        onShow={() => setActiveIndex(activeIndex === 0 ? undefined : 0)}
-      />
-      <Panel
-        title="Cascading Style Sheets"
-        descrip="Cascading Style Sheets (CSS) is a style sheet language used for describing
-                the presentation of a document written in a markup language such as HTML or XML
-                (including XML dialects such as SVG, MathML or XHTML). CSS is a cornerstone 
-                technology of the World Wide Web, alongside HTML and JavaScript."
-        isActive={activeIndex === 1}
-        onShow={() => setActiveIndex(activeIndex === 1 ? undefined : 1)}
-      />
-      <Panel
-        title="JavaScript"
-        descrip="JavaScript, often abbreviated as JS, is a programming language that is one of 
-                the core technologies of the World Wide Web, alongside HTML and CSS. As of 2022, 
-                98% of websites use JavaScript on the client side for webpage behavior, often 
-                incorporating third-party libraries. All major web browsers have a dedicated 
-                JavaScript engine to execute the code on devices."
-        isActive={activeIndex === 2}
-        onShow={() => setActiveIndex(activeIndex === 2 ? undefined : 2)}
-      />
+      <Container pages={pages} />
     </div>
   );
 }
 
-function Panel({ title, descrip, onShow, isActive, hidden = 'hidden' }) {
-  hidden = isActive ? '' : hidden;
+function Container({ pages }) {
+  const [activeIndex, setActiveIndex] = useState(undefined);
 
+  const panels = [];
+
+  for (let i = 0; i < pages.length; i++) {
+    panels.push(
+      <Panel
+        key={pages[i].key}
+        title={pages[i].title}
+        descrip={pages[i].descrip}
+        isActive={activeIndex === pages[i].key}
+        onShow={() => setActiveIndex(activeIndex === i ? undefined : i)}
+      />
+    );
+  }
+
+  return <div>{panels}</div>;
+}
+
+function Panel({ title, descrip, onShow, isActive }) {
+  let dropDown;
+  if (isActive) {
+    dropDown = <p>{descrip}</p>;
+  }
   return (
     <div className="Panel" onClick={onShow}>
       <div className="title">
         <h3>{title}</h3>
       </div>
-      <div className={`descrip ${hidden}`}>
-        <p>{descrip}</p>
-      </div>
+      <div className={`descrip`}>{dropDown}</div>
     </div>
   );
 }
 
-export default Container;
+export default App;
