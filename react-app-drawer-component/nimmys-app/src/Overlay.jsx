9ad3bbfb7da909398bf63b0ca62f7a06028b80b1
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-25 16:35:09 +0000

    self-closing tag

--- react-app-drawer-component/nimmys-app/src/Overlay.jsx
+++ react-app-drawer-component/nimmys-app/src/Overlay.jsx
@@ -1,7 +1,5 @@
 export default function Overlay({ isActive, onHide }) {
   const shade = isActive ? 'shady' : 'sunny';
 
-  return (
-    <div className={`overlay ${shade}`} onClick={() => onHide(false)}></div>
-  );
+  return <div className={`overlay ${shade}`} onClick={() => onHide(false)} />;
 }
