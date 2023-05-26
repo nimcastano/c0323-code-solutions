c259490c7c48d8065723f4c8ef4b3a984139040c
Nimrod Castano <nimrodcastano@gmail.com>
2023-05-26 19:43:03 +0000

    updated for error handling

--- fetch/main.js
+++ fetch/main.js
@@ -1,7 +1,14 @@
 async function JSONData() {
-  const response = await fetch('https://jsonplaceholder.typicode.com/users');
-  const jsonData = await response.json();
-  console.log(jsonData);
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/users');
+    if (!response.ok) {
+      throw new Error(`fetch Error ${response.status}`);
+    }
+    const jsonData = await response.json();
+    console.log(jsonData);
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 JSONData();
