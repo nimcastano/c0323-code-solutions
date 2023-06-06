09f89443afdcb57faa1be84944a371a91c0e6302
Nimrod Castano <nimrodcastano@gmail.com>
2023-06-06 19:15:32 +0000

    updated

--- big-o-notation/big-o.js
+++ big-o-notation/big-o.js
@@ -22,25 +22,25 @@ function uniqueQuadratic(words) {
   const unique = []; // 1 * 1 = O(1)
   for (
     let i = 0; // 1 * 1 = O(1)
-    i < words.length; // 1 * n = O(n)
+    i < words.length; // 2 * n = O(2n)
     i++ // 2 * n = O(2n)
   ) {
     const word = words[i]; // 2 * n = O(2n)
     let isUnique = true; // 1 * n = O(n)
     for (
       let c = 0; // 1 * n = O(n)
-      c < i; // 2 * n * n = O(2n^2)
+      c < i; // n * n = O(n^2)
       c++ // 2 * n * n = O(2n^2)
     ) {
-      const comparing = words[c]; // n * n = O(n^2)
+      const comparing = words[c]; // 2 * n * n = O(2n^2)
       if (comparing === word) {
-        // 3 * n * n = O(3n^2)
+        // n * n = O(n^2)
         isUnique = false; // n * n = O(n^2)
       }
     }
     if (isUnique) {
       // 1 * n = O(n)
-      unique[unique.length] = word; // 2 * n * n = O(2n^2)
+      unique[unique.length] = word; // 2 * n = O(2n)
     }
   }
   return unique; // 1 * 1 = O(1)
