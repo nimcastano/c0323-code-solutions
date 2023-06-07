612e91fc7a4cf2bf51c30e4031393bb81355dd3c
Nimrod Castano <nimrodcastano@gmail.com>
2023-06-07 17:28:42 +0000

    completed data-structures-stacks

--- data-structures-stacks/get-2nd-from-top.js
+++ data-structures-stacks/get-2nd-from-top.js
@@ -1,13 +1,5 @@
 export default function get2ndFromTop(stack) {
-  let count = 1;
   if (stack.peek() === undefined) {
     return undefined;
-  }
-  while (stack.peek() !== undefined) {
-    if (count === 0) {
-      return stack.pop();
-    }
-    stack.pop();
-    count--;
   }
 }
