1272b569fe685a6374e3d7b8b6f009e3d8ffad4b
Nimrod Castano <nimrodcastano@gmail.com>
2023-06-13 19:20:22 +0000

    completed javascript-closures exercise

--- javascript-closures/my-app/src/Counter.js
+++ javascript-closures/my-app/src/Counter.js
@@ -2,16 +2,14 @@ import React, { useCallback, useEffect, useState } from 'react';
 
 export default function Counter() {
   const [counter, setCounter] = useState(0);
+
   useEffect(() => {
     setCounter((prev) => prev + 1);
   }, []);
 
-  function getData() {
-    // fetch data
+  useCallback(() => {
     return { foo: 'bar' };
-  }
-
-  useCallback(() => getData(), []);
+  }, []);
 
   return <div>Fetching {counter} times</div>;
 }
